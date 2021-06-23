b0d279fbe4c84cb3ce66f8f774b0e73392b44bb7
Akash Das <mfs.akash@gmail.com>
2021-06-23 21:09:52 +0530

    Javascript concept demonstration

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,41 @@
-const array = [10, 20, 30, 40];
+let car1 = "Volvo";
+let car2 = "BMW";
+let car3 = "Audi";
 
-for (let element of array) {
-  console.log(element + 1);
+let cars = ["Volvo", "BMW", "Audi"];
+
+for (let car of cars) {
+  console.log(car);
 }
+
+let x = 5;
+var y = 8.576;
+let name = "Akash";
+
+let empID;
+let salary = null;
+
+function run() {
+  let bar;
+
+  console.log(bar);
+
+  var foo = "Foo";
+  bar = "Bar";
+
+  console.log(foo, bar);
+
+  {
+    var moo = "Mooo";
+    let baz = "Bazz";
+    // console.log(moo, baz);
+  }
+
+  // console.log(moo);
+  // console.log(baz);
+}
+
+run();
+
+console.log(moo);
+console.log(baz);
