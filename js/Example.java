c22efe1469eb48fac2127a510d3bebad5b29c8dd
Akash Das <mfs.akash@gmail.com>
2021-07-12 21:41:14 +0530

    Added demo Java file for comparision

---
+++ js/Example.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+        int z = 20;
+
+        System.out.println(y);
+
+        if (20 == z) {
+            int y = 10;
+        }
+
+        System.out.print(x);
+    }
+}
